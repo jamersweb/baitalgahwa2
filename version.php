4755434664035a19a70804c2ad4834958593e598
siddiquitaha867-ai <siddiquitaha867@gmail.com>
2026-06-02 23:26:42 +0500

    Make my dashboard CSS Moodle compiler safe

--- version.php
+++ version.php
@@ -4,7 +4,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_baitulghawa';
-$plugin->version = 2026060201;
+$plugin->version = 2026060202;
 $plugin->requires = 2022112800;
-$plugin->release = '1.0.1';
+$plugin->release = '1.0.2';
 $plugin->maturity = MATURITY_STABLE;
