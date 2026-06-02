3c438e9edf5a7fd40775c2dac1f2b7af6a649bc9
siddiquitaha867-ai <siddiquitaha867@gmail.com>
2026-06-03 00:37:29 +0500

    Inject dashboard CSS through theme renderer

--- version.php
+++ version.php
@@ -4,7 +4,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_baitulghawa';
-$plugin->version = 2026060300;
+$plugin->version = 2026060301;
 $plugin->requires = 2022112800;
-$plugin->release = '1.0.6';
+$plugin->release = '1.0.7';
 $plugin->maturity = MATURITY_STABLE;
