5d83731e36941fc3af5416c762a17300e3ce6f22
siddiquitaha867-ai <siddiquitaha867@gmail.com>
2026-06-02 23:42:02 +0500

    Import theme styles through default preset

--- version.php
+++ version.php
@@ -4,7 +4,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_baitulghawa';
-$plugin->version = 2026060203;
+$plugin->version = 2026060204;
 $plugin->requires = 2022112800;
-$plugin->release = '1.0.3';
+$plugin->release = '1.0.4';
 $plugin->maturity = MATURITY_STABLE;
