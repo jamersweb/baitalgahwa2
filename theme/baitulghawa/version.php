691b5c6cf65ca0d12a131e1f228bcc048ebf3d46
siddiquitaha867-ai <siddiquitaha867@gmail.com>
2026-06-02 21:25:29 +0500

    Lower Baitulghawa Moodle requirement

--- theme/baitulghawa/version.php
+++ theme/baitulghawa/version.php
@@ -5,6 +5,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_baitulghawa';
 $plugin->version = 2026060200;
-$plugin->requires = 2026051200;
+$plugin->requires = 2022112800;
 $plugin->release = '1.0.0';
 $plugin->maturity = MATURITY_STABLE;
